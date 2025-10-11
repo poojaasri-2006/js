cc2ee499cbb2c5b7fa52484c017245e55fa6edc8
poojaasri-2006 <psri662006@gmail.com>
2025-10-11 16:24:21 +0000

    var and datatypes done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 166668
+let accountEmail ="psree662006@gmail.com"
+var accountPass ="12345678"
+accountCity ="Hyd"
+accountId = 2
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+//alert(3+3) 
+
+let name = "pooja"
+let age = 19
+let isLoggedIn = false
+console.log()
